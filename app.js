1030a6c1eba0dbf84467f6b6ac8e536cb35619d0
Sashreek-PC <sashreek102003@gmail.com>
2024-03-17 22:29:20 +0530

    Empty-Commit

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
 const expressLayout = require('express-ejs-layouts');
-const connectDB=require('./server/config/db');
+const connectDB = require('./server/config/db');
 const PORT = 5000 || process.env.PORT;
 
 connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 //templaing engine;
 app.use(expressLayout);
-app.set('layout','./layouts/main');
-app.set('view engine','ejs');
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 
 
-app.use('/',require('./server/routes/main'));
-app.use('/',require('./server/routes/admin'));
-app.listen(PORT,()=>{
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
+app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
 });
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
-// app.use(express.json()); 
-// app.use(express.urlencoded({ extended: true })); 
-const adminLayout='../views/layouts/admin';
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const adminLayout = '../views/layouts/admin';
 // GET Admin Login-Page 
 
 router.get('/admin', async (req, res) => {
@@ -14,7 +14,7 @@ router.get('/admin', async (req, res) => {
             title: "Admin",
             description: "Simple Blog created with NodeJs, Express & MongoDb."
         }
-        res.render('admin/index', { locals,layout:adminLayout});
+        res.render('admin/index', { locals, layout: adminLayout });
     } catch (error) {
         console.log(error);
     }
@@ -29,12 +29,36 @@ router.get('/admin', async (req, res) => {
 router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body;
-        console.log(req.body)
+        console.log(req.body);
         res.redirect('/admin');
-    } 
-    
-    
+    }
+
+
     catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+//POST Admin-Register
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({ username, password: hashedPassword });
+            res.status(201).json({ message: 'User Created', user });
+        } catch (error) {
+            if (error.code === 11000) {
+                res.status(409).json({ message: 'User already in use' });
+            }
+            res.status(500).json({ message: 'Internal server error' })
+        }
+
+    } catch (error) {
         console.log(error);
     }
 });
