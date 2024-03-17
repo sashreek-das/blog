4829146f37de718f3dca6b52cc6799fb3950de03
Sashreek-PC <sashreek102003@gmail.com>
2024-03-17 16:49:07 +0530

    Empty-Commit

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const Post = require('../models/Post');
-
-
+const User = require('../models/User');
+// app.use(express.json()); 
+// app.use(express.urlencoded({ extended: true })); 
+const adminLayout='../views/layouts/admin';
 // GET Admin Login-Page 
 
 router.get('/admin', async (req, res) => {
@@ -12,11 +14,28 @@ router.get('/admin', async (req, res) => {
             title: "Admin",
             description: "Simple Blog created with NodeJs, Express & MongoDb."
         }
-        res.render('admin/index', { locals});
+        res.render('admin/index', { locals,layout:adminLayout});
     } catch (error) {
         console.log(error);
     }
 
 });
+
+
+
+
+// POST Admin-checkLogin
+
+router.post('/admin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log("Username:", username);
+        console.log("Password:", password);
+        res.redirect('/admin');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 module.exports = router;
